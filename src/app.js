ee26ad0d0efb4511f407c86d16640fd8300539fd
kenny-67 <kennynnopu@gmail.com>
2020-06-20 17:23:57 +0100

    responsiveness test 2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import "./styles/bootstrap.min.css";
+import "./styles/bootstrap.min.css";
 import "./styles/style.css";
 
 import bot from "./images/bot.jpg";
